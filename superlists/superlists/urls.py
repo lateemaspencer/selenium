4eead5ee69207ae1a737b6c80b7ce51ccb5adf03
lateemaspencer <spencer.lateema@gmail.com>
2017-01-30 13:32:15 -0500

    Switch to using unittest for fts, extend with user story

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
+from lists.views import home_page
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', home_page, name='home'),
+    #url(r'^admin/', include(admin.site.urls))
 ]
