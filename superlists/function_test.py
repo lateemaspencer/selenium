4e3616263f942587e28e263fe522a0a0e4512fc0
lateemaspencer <spencer.lateema@gmail.com>
2017-02-02 21:01:39 -0500

    add implicit wait to ft

--- superlists/function_test.py
+++ superlists/function_test.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -27,11 +28,12 @@ class NewVisitorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         #is tying fly-fishing lures)
-        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys("Buy Peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -45,6 +47,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
         table = self.browser.find_element_by_id('id_list_table')
@@ -68,4 +71,4 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(warnings='ignore')
