d371f88a42bda6f38c9768504f6e533e18fa9186
lateemaspencer <spencer.lateema@gmail.com>
2017-02-02 21:02:09 -0500

    start towards processing post requests on server

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 
+
 # Create your tests here.
 
 class HomePageTest(TestCase):
 
     def test_home_page_is_about_todo_lists(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
